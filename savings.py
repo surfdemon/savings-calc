49c395be55ab8575bdee1a816a5f3680eaf4b3f1
Rob <surfdemon@gmail.com>
2016-01-08 12:43:44 +0000

    fixed, now says 1.05 instead of 1.0 when it gets to 14th day

--- savings.py
+++ savings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.4
-
 from decimal import *
 from datetime import datetime
+
 
 startingSavingAmount = Decimal('0.00')
 incrementAmount = Decimal('0.00')
@@ -9,7 +9,6 @@ totalAmount = Decimal('0.00')
 
 
 def __init__():
-    getcontext().prec = 2
     global startingSavingAmount 
     startingSavingAmount = Decimal('0.01')
     global incrementAmount
@@ -22,15 +21,12 @@ def amountToSaveToday():
     global totalAmount
     startamount = startingSavingAmount 
     x = 0
-    fmt = '{0:<6} {1:<6}'
-    print(fmt.format('save', 'total'))
-    print(fmt.format('-' * 6, '-' * 6))
-    for x in range(0, 365):
+    for x in range(0, day):
         totalAmount = Decimal(totalAmount) + Decimal(startamount)
-        print(fmt.format(startamount, totalAmount))
         startamount = Decimal(startamount) + Decimal(incrementAmount)
         x + 1
-    print('This is the amount to save today - ' + str(totalAmount))
+    print('Today save: £' + str(Decimal(startamount) - Decimal('0.01') ) + ' You should now have a total of: £' + str(totalAmount))
+
 
 if __name__ == "__main__":
     __init__()
